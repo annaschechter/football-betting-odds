111c56988ff007d89235c20da3add27a7d88975c
annajerofejeva <jerofejeva.anna@yahoo.com>
2015-01-10 11:26:13 +0000

    Added odds object

---
+++ test/spec/oddsSpec.js
@@ -0,0 +1,22 @@
+var Odds = require('../../lib/odds.js');
+
+describe("Odds", function() {
+
+  var odds;
+  beforeEach(function(){
+    odds = new Odds(1.23, 13.6, 5);
+  });
+
+  it('odds know the price for team1 to win', function() {
+    expect(odds.team1Win).toEqual(1.23);
+  });
+
+  it('odds know the price for team1 to loose', function() {
+    expect(odds.team1Loose).toEqual(13.6);
+  });
+
+  it('odds know the price for team1 to win', function() {
+    expect(odds.draw).toEqual(5);
+  })
+
+}); 

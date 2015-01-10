e8716b132448c59e559e12c749e30076a60710c1
annajerofejeva <jerofejeva.anna@yahoo.com>
2015-01-10 11:54:43 +0000

    Match can store odds

--- test/spec/matchSpec.js
+++ test/spec/matchSpec.js
@@ -3,8 +3,10 @@ var Match = require('../../lib/match.js');
 describe("Match", function() {
 
   var match;
+  var odds;
   beforeEach(function(){
     match = new Match(27340168, "Espanyol", "Barcelona");
+    odds = {};
   });
 
   it("knows it's betfair event id", function() {
@@ -19,7 +21,13 @@ describe("Match", function() {
     expect(match.team2).toEqual("Barcelona");
   });
 
+  it("can store the odds", function() {
+    expect(match.odds.length).toEqual(0);
+  });
 
-
+  it("can record odds", function() {
+    match.updateOdds(odds);
+    expect(match.odds.length).toEqual(1)
+  });
 
 })
